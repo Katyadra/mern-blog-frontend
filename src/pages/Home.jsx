37eee6b4599a3e9580cdd26d4d4d662e27991591
Katyadra <Katyakhorrgh@gmail.com>
2024-05-30 16:56:05 +0300

    suffer

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,13 +1,23 @@
 import React from 'react';
 import Tabs from '@mui/material/Tabs';
+import {useDispatch, useSelector } from 'react-redux';
 import Tab from '@mui/material/Tab';
 import Grid from '@mui/material/Grid';
-
 import { Post } from '../components/Post';
 import { TagsBlock } from '../components/TagsBlock';
 import { CommentsBlock } from '../components/CommentsBlock';
-
+import { fetchPosts } from '../redux/slices/posts';
 export const Home = () => {
+  const dispatch = useDispatch(); 
+  const {posts, tags} = useSelector(state => state.posts);
+  const isPostsLoading = posts.status === 'loading';
+
+  React.useEffect(() => {
+    dispatch(fetchPosts());
+  }, []);
+
+  console.log(posts);
+
   return (
     <>
       <Tabs style={{ marginBottom: 15 }} value={0} aria-label="basic tabs example">
@@ -16,10 +26,13 @@ export const Home = () => {
       </Tabs>
       <Grid container spacing={4}>
         <Grid xs={8} item>
-          {[...Array(5)].map(() => (
+          {(isPostsLoading ? [...Array(5)] : posts.items).map((obj, index) => 
+          isPostsLoading ? (
+          <Post key = {index} isLoading={true} /> 
+        ) : (
             <Post
-              id={1}
-              title="Roast the code #1 | Rock Paper Scissors"
+              id={obj._id}
+              title= {obj.title}
               imageUrl="https://res.cloudinary.com/practicaldev/image/fetch/s--UnAfrEG8--/c_imagga_scale,f_auto,fl_progressive,h_420,q_auto,w_1000/https://dev-to-uploads.s3.amazonaws.com/uploads/articles/icohm5g0axh9wjmu4oc3.png"
               user={{
                 avatarUrl:
@@ -32,7 +45,8 @@ export const Home = () => {
               tags={['react', 'fun', 'typescript']}
               isEditable
             />
-          ))}
+            ),
+          )}
         </Grid>
         <Grid xs={4} item>
           <TagsBlock items={['react', 'typescript', 'заметки']} isLoading={false} />
